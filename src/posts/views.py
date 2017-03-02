b6be3b7dfbc54dacee4edb0e6837ff6960fc95c4
devayansarkar <devayansarkar@gmail.com>
2017-03-02 18:58:38 +0100

    rendering html with dynamic values

--- src/posts/views.py
+++ src/posts/views.py
@@ -7,10 +7,24 @@ def posts_create(request):
     return HttpResponse("<h1> Create </h1>")
     
 def posts_detail(request):
-    return HttpResponse("<h1> Detail </h1>")
+    context_data={
+        "title": "Detail"
+    }
+    return render(request,"index.html",context_data)
 
 def posts_list(request):
-    return render(request,"index.html",{})
+   # if request.user.is_authenticated():
+    #     context_data={
+    #          "title": "My User List"
+    #      }
+    # else:
+    #     context_data={
+    #     "title": "unauthernticated User List"
+    #      }
+    context_data={
+         "title": "List is working"
+          }
+    return render(request,"index.html",context_data)
 
 def posts_update(request):
     return HttpResponse("<h1> Update </h1>")
